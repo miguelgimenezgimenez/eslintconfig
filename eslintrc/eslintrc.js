8123702e5ae411db17364b9e27a7c83442d42db6
Miguel Gimenez <gimenezgimenezmiguel@gmail.com>
2018-08-24 18:29:10 +0200

    Create eslintrc.js

---
+++ eslintrc/eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint-config-react-native",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "react-native/no-color-literals": "false",
+        "react/prefer-stateless-function": 0,
+        "react/prop-types": 0,
+        "class-methods-use-this": "off",
+        "no-console":"off",
+        "indent": [
+            "error",
+            3
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
